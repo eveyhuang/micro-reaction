9cad4a4eb38e367439c5545d32a55bff93d1b6d4
eveyhuang <jiaxinh.evey@gmail.com>
2018-08-22 13:02:50 +0900

    changed most of post.js to stateless by having attributes passed down as props from app.js

--- micro-reaction/src/App.js
+++ micro-reaction/src/App.js
@@ -4,12 +4,54 @@ import Post from './Post/Post';
 import './App.css';
 import { Button, Comment, Form, Header, Label, List } from 'semantic-ui-react'
 
+const metrics= [
+  {
+      name: 'Fairness',
+      description: 'Reflects values about balanced representation of issues.',
+      color: 'red',
+      count: 0,
+  },
+  {
+      name: 'Novelty',
+      description: 'Offers unique or novel insights, ideas, opinions or stories.',
+      color: 'blue',
+      count: 0,
+  },
+  {
+      name: 'Personal Experience',
+      description:'Contains the writers personal observations or historical perspectives.',
+      color: 'yellow',
+      count: 0,
+  },
+  {
+      name: 'Argument Quality',
+      description: 'Expresses a well-grounded and justifiable argument that warrants claims with evidence.',
+      color: 'green',
+      count: 0,
+  },
+  {
+      name: 'Readability',
+      description: 'Well articulated and written in terms of style, clarity, and adherence to standard grammar. ',
+      color: 'purple',
+      count: 0,
+  }]
+
 class App extends Component {
   
-
-  incCount(){
-    djfdksjfksjdf
+  state ={
+      posts:[
+        {author:'cg', 
+        avatar: 'https://react.semantic-ui.com/images/avatar/small/joe.jpg' ,
+        date:'Oct.27, 2017', 
+        text: "I honestly don't know how my Republican friends can still call themselves Republicans. In  supporting this president the true essence of the Republican voter is revealed and it is not pretty.  $175,000,000.oo to renovate the White House...fine. Voting against class action suits against banks that have broken the law...fine.  Going back on our word and agreements with our allies....fine. Throwing out the children of immigrants even if born here...fine.  Doing away with special education...fine.  Tormenting young widow of fallen soldier...fine. Doing away with clean air and water...fine. Cutting a trillion dollars out of medicaid and medicare...fine..and on and on.  The things that they are ok with define them...why would anyone with a social concious want anything to do with them? ",
+        metrics: metrics},
+      ],
   }
+
+  incCount(metricName){
+    return null
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,34 +60,11 @@ class App extends Component {
         <Header as='h3' dividing>
                     Comments
         </Header>
-      {/*   <List horizontal>
-          <List.Item>
-            <Label circular color='red' />
-            <List.Content>
-              <List.Header>Fairness</List.Header>
-              Top Contributor
-            </List.Content>
-          </List.Item>
-          <List.Item>
-          <Label circular color='yellow' />
-            <List.Content>
-              <List.Header>Novelty</List.Header>
-              Admin
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <Label circular color='green' />
-            <List.Content>
-              <List.Header>Readability</List.Header>
-              Top Rated User
-            </List.Content>
-          </List.Item>
-        </List>
- */}
-  {posts.map(post => (
-    <Post data={post} incCount={this.incCount}/>
-  ))}
-        <Post/>
+
+      {this.state.posts.map(post => (
+        <Post data={post} incCount={this.incCount}/>
+      ))}
+        
       </div>
     );
   }
--- micro-reaction/src/Post/Post.js
+++ micro-reaction/src/Post/Post.js
@@ -2,65 +2,25 @@ import React from 'react';
 import { Button, Comment, Form, Header, Icon, Popup, Grid, Container, Dropdown, Label, Statistic} from 'semantic-ui-react'
 
 
-const metrics = [
-    {
-        name: 'Fairness',
-        description: 'Reflects values about balanced representation of issues.',
-        color: 'red',
-        count: 0,
-    },
-    {
-        name: 'Novelty',
-        description: 'Offers unique or novel insights, ideas, opinions or stories.',
-        color: 'blue',
-        count: 0,
-    },
-    {
-        name: 'Personal Experience',
-        description:'Contains the writers personal observations or historical perspectives.',
-        color: 'yellow',
-        count: 0,
-    },
-    {
-        name: 'Argument Quality',
-        description: 'Expresses a well-grounded and justifiable argument that warrants claims with evidence.',
-        color: 'green',
-        count: 0,
-    },
-    {
-        name: 'Readability',
-        description: 'Well articulated and written in terms of style, clarity, and adherence to standard grammar. ',
-        color: 'purple',
-        count: 0,
-    }
-]
-
 const Post = (props) => {
-    
-    changeCount =(metricName)=> {
-        
-    };
-    
     return (
+        
         <Container>
             
             <Comment.Group threaded>
                 
             <Comment>
-                <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+                <Comment.Avatar as='a' src={props.data.avatar} />
                 <Comment.Content>
 
-                    <Comment.Author>Tom Lukic</Comment.Author>
+                    <Comment.Author>{props.data.author}</Comment.Author>
                     <Comment.Metadata>
-                    <span>Today at 5:42PM</span>
+                    <span>{props.data.date}</span>
                     </Comment.Metadata>
-                    <Comment.Text>
-                    I honestly don't know how my Republican friends can still call themselves Republicans. In  supporting this president the true essence of the Republican voter is revealed and it is not pretty.  $175,000,000.oo to renovate the White House...fine. Voting against class action suits against banks that have broken the law...fine.  Going back on our word and agreements with our allies....fine. Throwing out the children of immigrants even if born here...fine.  Doing away with special education...fine.  Tormenting young widow of fallen soldier...fine.
-Doing away with clean air and water...fine. Cutting a trillion dollars out of medicaid and medicare...fine..and on and on.  The things that they are ok with define them...why would anyone with a social concious want anything to do with them?
-                    </Comment.Text>
-                    <Comment.Actions>
+                    <Comment.Text> {props.data.text}</Comment.Text>
+                     <Comment.Actions>
                         <Comment.Action> 
-                            {metrics.map(metric => (
+                            {props.data.metrics.map(metric => (
                                 <Statistic color={metric.color} size='mini'>
                                 
                                     <Statistic.Value>
@@ -79,7 +39,7 @@ Doing away with clean air and water...fine. Cutting a trillion dollars out of me
                             ))}
                         
                         </Comment.Action>
-                    </Comment.Actions>
+                    </Comment.Actions> 
                 </Comment.Content>
             </Comment>
         </Comment.Group>

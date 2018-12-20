8270590f686a0d3929248c06e092bf02945ab4de
Evey Huang <jiaxinh.evey@gmail.com>
2018-12-20 15:02:51 +0900

    gonna change modal to class now

--- micro-reaction/src/Modal/Modal.js
+++ micro-reaction/src/Modal/Modal.js
@@ -1,20 +1,29 @@
 import React from 'react';
+
 import { Button, Dropdown, Container, Header, Message } from 'semantic-ui-react'
 
 let selectedCategory=[]
 
 
+
 const Modal = ({ handleSubmit, handleClose, show, post, categ }) => {
-    
-    const showHideClassName = show ? "modal display-block" : "modal display-none";
-    
-    const setCategories = (event, {value})=>{
-      selectedCategory=value;
+
+    state={
+      displayVal:post.categories
     }
 
-    const submitCateg =()=>{
+    const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+    const setCategories = (event, {value})=>{
+      console.log(value);
+      selectedCategory=value;
+      this.setState({displayVal:value})
+    }
+
+    const submitCateg=()=>{
       handleSubmit(post.id, selectedCategory)
       handleClose()
+     
       selectedCategory=[];
     }
 
@@ -32,7 +41,7 @@ const Modal = ({ handleSubmit, handleClose, show, post, categ }) => {
           </Message>
           
           
-          <Dropdown placeholder='Categories' fluid multiple selection closeOnChange options={categ} onChange = {setCategories} />
+          <Dropdown placeholder='Categories' fluid multiple selection closeOnChange options={categ} value={this.state.displayVal} onChange = {setCategories} />
           <Button onClick={submitCateg}>Submit</Button>
           <Button onClick={handleClose}>Close</Button>
         </Container>

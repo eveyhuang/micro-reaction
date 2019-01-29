a2949e69c4c1c13f42ce350ce0de1d78ab4b9d02
KIONLEE <>
2019-01-29 12:01:33 +0900

    formatting each post on postwithUpvote as reddit

--- micro-reaction/src/PostwithUpvotes/PostwithUpvotes.js
+++ micro-reaction/src/PostwithUpvotes/PostwithUpvotes.js
@@ -8,6 +8,25 @@ import {
   Header
 } from "semantic-ui-react";
 
+import enLocale from "date-fns/locale/en";
+import differenceInDays from "date-fns/difference_in_days";
+import distanceInWords from "date-fns/distance_in_words";
+import format from "date-fns/format";
+
+function getFormattedDate(d) {
+  const now = new Date();
+  const date = new Date(d);
+
+  // Less then 1 min
+  if (now - date < 60 * 1000) return "Just now";
+  // Less then 10 days
+  if (differenceInDays(now, date) < 10) {
+    return distanceInWords(now, date, { locale: enLocale, addSuffix: true });
+  }
+  // otherwise YYYY-MM-DD
+  return format(date, "YYYY-MM-DD");
+}
+
 const PostwithUpvotes = props => {
   return (
     <Grid columns={2}>
@@ -40,8 +59,17 @@ const PostwithUpvotes = props => {
             <Header as="h3" dividing>
               {props.data.title}
             </Header>
-            <Comment.Author>{props.data.user}</Comment.Author>
-            <Comment.Text>{props.data.content}</Comment.Text>
+            <Comment.Text style={{ marginBottom: "0.5rem" }}>
+              {props.data.content}
+            </Comment.Text>
+            <Comment.Author>
+              <text style={{ fontWeight: "bold" }}>{props.data.user}</text>
+              <text style={{ opacity: "0.5" }}>
+                {` • Posted at ${getFormattedDate(
+                  props.data.createdAt.toDate()
+                )}`}
+              </text>
+            </Comment.Author>
           </Comment.Content>
         </Comment>
       </Grid.Column>
